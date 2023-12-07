b0492e5b46a0772a3acf29c79faecf15039f7052
Joshua Wallington <Jdwalli2003@yahoo.com>
2023-12-07 00:27:59 -0500

    Solved 2023 Day 4

--- advent-of-code/2023/Day4/solution.py
+++ advent-of-code/2023/Day4/solution.py
@@ -1,5 +1,6 @@
 
 separator = '|'
+
 def calculate_card_points(card):
     winning_values, elf_values = card.split(": ")[1].split(separator)
     winning_values = [int(n) for n in winning_values.split()]
@@ -12,50 +13,30 @@ def calculate_card_points(card):
 
     return 2 ** (matches - 1) if matches > 0 else 0
 
-def calculate_additional_cards(card):
-    card_number = int(card.split(": ")[0][4:])
-    winning_values, elf_values = card.split(": ")[1].split(separator)
-    winning_values = [int(n) for n in winning_values.split()]
-    elf_values = [int(n) for n in elf_values.split()]
-    matches = 0
-    cards_won = []
-
-    
-    for value in elf_values:
-        if value in winning_values:
-            matches += 1
-
-    print(f"Matches: {matches}")
-    print(f"Card Number: {card_number}")
-    for i in range(card_number, card_number + matches + 1):
-        cards_won.append(i)
-        
-
-    print(cards_won, end='\n')
-    return cards_won
-
 def solve_part_one(scratchcard_data):
     score = 0
     for card in scratchcard_data:
         score += calculate_card_points(card)
-     
     return score
 
 
 def solve_part_two(scratchcard_data): 
-    additional_cards_won = []
-    for card in scratchcard_data:
-        additional_cards_won.extend(calculate_additional_cards(card))
-    
-    print(additional_cards_won)
-
-    return sum(additional_cards_won)
-
-
+    counts = [1] * len(scratchcard_data)
+    for index, card in enumerate(scratchcard_data):
+        winning_values, elf_values = card.split(": ")[1].split(separator)
+        winning_values = [int(n) for n in winning_values.split()]
+        elf_values = [int(n) for n in elf_values.split()]
+        num_matches = len([c for c in elf_values if c in winning_values])
+        first_index = index + 1
+        for i in range(first_index, first_index + num_matches):
+            counts[i] += counts[index]
+    return sum(counts)
 
 
 if __name__ == "__main__":
     with open('input.txt', 'r') as scratchcards:
         scratchcard_data = scratchcards.readlines()
-        # print(solve_part_one(scratchcard_data))
+        assert solve_part_one(scratchcard_data) == 21568
+        assert solve_part_two(scratchcard_data) == 11827296
+        print(solve_part_one(scratchcard_data))
         print(solve_part_two(scratchcard_data))
